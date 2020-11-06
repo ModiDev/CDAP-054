41f43b680e544c0c25d415fefe924b1dd1bd7a41
ruwinTharanga <47855588+ruwinTharanga@users.noreply.github.com>
2020-11-06 22:17:31 +0530

    Update RSP_L2NNMoveSeane.cs

--- Assets/Road Safety Finsl/Road predastion/Scrips/RSP_L2NNMoveSeane.cs
+++ Assets/Road Safety Finsl/Road predastion/Scrips/RSP_L2NNMoveSeane.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Change Scene using Scene name..
 public class RSP_L2NNMoveSeane : MonoBehaviour
 {
     [SerializeField] private string RSP_loadLevel;
@@ -11,7 +12,7 @@ public class RSP_L2NNMoveSeane : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(RSP_loadLevel);
+            SceneManager.LoadScene(RSP_loadLevel); // Change Scenes
         }
     }
 }
