189d30788ec2058c734aadbf40a08d26b81e612a
ruwinTharanga <47855588+ruwinTharanga@users.noreply.github.com>
2020-11-06 22:00:58 +0530

    Update RSP_L1CollCoCorr.cs

--- Assets/Road Safety Finsl/Road predastion/Scrips/RSP_L1CollCoCorr.cs
+++ Assets/Road Safety Finsl/Road predastion/Scrips/RSP_L1CollCoCorr.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Object selected..
 public class RSP_L1CollCoCorr : MonoBehaviour
 {
     public AudioSource collectSound;
 
     void OnTriggerEnter(Collider other)
     {
-        collectSound.Play();
-        RSP_L1Score.theScore -= 50;
-        Destroy(gameObject);
+        collectSound.Play(); //Object collected sound
+        RSP_L1Score.theScore -= 50; //Marks are deducted
+        Destroy(gameObject); // And Destroy Objects
 
     }
 }
