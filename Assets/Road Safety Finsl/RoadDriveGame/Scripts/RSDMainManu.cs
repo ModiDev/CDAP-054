accc474e2eaea826e67618496e4bcdd60112f520
ruwinTharanga <47855588+ruwinTharanga@users.noreply.github.com>
2020-11-06 22:42:28 +0530

    Update RSDMainManu.cs

--- Assets/Road Safety Finsl/RoadDriveGame/Scripts/RSDMainManu.cs
+++ Assets/Road Safety Finsl/RoadDriveGame/Scripts/RSDMainManu.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+ //Change scene
 public class RSDMainManu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3); //Change scene index 3
     }
     public void QuitGame()
     {
