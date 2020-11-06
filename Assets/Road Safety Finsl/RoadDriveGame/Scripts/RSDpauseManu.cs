ef0af523e0bfb54863797f582f9b17538a6df345
ruwinTharanga <47855588+ruwinTharanga@users.noreply.github.com>
2020-11-06 22:46:22 +0530

    Update RSDpauseManu.cs

--- Assets/Road Safety Finsl/RoadDriveGame/Scripts/RSDpauseManu.cs
+++ Assets/Road Safety Finsl/RoadDriveGame/Scripts/RSDpauseManu.cs
@@ -6,17 +6,17 @@ public class RSDpauseManu : MonoBehaviour
 {
     public GameObject Pausemenu, PauseButton; 
 
-    public void Pause()
+    public void Pause() //Create Pause Menu 
     {
-        Pausemenu.SetActive(true);
-        PauseButton.SetActive(false);
+        Pausemenu.SetActive(true); 
+        PauseButton.SetActive(false); //Using Pause Button
         Time.timeScale = 0;
     }
 
-    public void Resume()
+    public void Resume() 
     {
         Pausemenu.SetActive(false);
-        PauseButton.SetActive(true);
+        PauseButton.SetActive(true); //Using Resume Button
         Time.timeScale = 1;
     }
 }
